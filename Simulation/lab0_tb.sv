e0fddc60de589128de6e77b2b447c28f27e537e5
Levi Johanon <ljohanon06@gmail.com>
2026-08-21 10:08:02 -0500

    Complete lab 0

--- Simulation/lab0_tb.sv
+++ Simulation/lab0_tb.sv
@@ -10,6 +10,7 @@ module lab0_tb;
     logic XOR_OUT;
     logic NAND_OUT;
     logic NOR_OUT;
+	 logic XNOR_OUT;
 
     // Instantiate the design under test
     lab0 dut (
@@ -19,41 +20,42 @@ module lab0_tb;
         .OR_OUT(OR_OUT),
         .XOR_OUT(XOR_OUT),
         .NAND_OUT(NAND_OUT),
-        .NOR_OUT(NOR_OUT)
+        .NOR_OUT(NOR_OUT),
+		  .XNOR_OUT(XNOR_OUT)
     );
 
     initial begin
 
-        $display("SW0 SW1 | AND OR XOR NAND NOR");
+        $display("SW0 SW1 | AND OR XOR NAND NOR XNOR");
         $display("--------------------------------");
 
         // Test 1: SW0 = 0, SW1 = 0
         SW0 = 0;
         SW1 = 0;
         #10;
-        $display(" %b   %b  |  %b   %b   %b    %b    %b",
-                 SW0, SW1, AND_OUT, OR_OUT, XOR_OUT, NAND_OUT, NOR_OUT);
+        $display(" %b   %b  |  %b   %b   %b    %b    %b    %b",
+                 SW0, SW1, AND_OUT, OR_OUT, XOR_OUT, NAND_OUT, NOR_OUT, XNOR_OUT);
 
         // Test 2: SW0 = 0, SW1 = 1
         SW0 = 0;
         SW1 = 1;
         #10;
-        $display(" %b   %b  |  %b   %b   %b    %b    %b",
-                 SW0, SW1, AND_OUT, OR_OUT, XOR_OUT, NAND_OUT, NOR_OUT);
+        $display(" %b   %b  |  %b   %b   %b    %b    %b    %b",
+                 SW0, SW1, AND_OUT, OR_OUT, XOR_OUT, NAND_OUT, NOR_OUT, XNOR_OUT);
 
         // Test 3: SW0 = 1, SW1 = 0
         SW0 = 1;
         SW1 = 0;
         #10;
-        $display(" %b   %b  |  %b   %b   %b    %b    %b",
-                 SW0, SW1, AND_OUT, OR_OUT, XOR_OUT, NAND_OUT, NOR_OUT);
+        $display(" %b   %b  |  %b   %b   %b    %b    %b    %b",
+                 SW0, SW1, AND_OUT, OR_OUT, XOR_OUT, NAND_OUT, NOR_OUT, XNOR_OUT);
 
         // Test 4: SW0 = 1, SW1 = 1
         SW0 = 1;
         SW1 = 1;
         #10;
-        $display(" %b   %b  |  %b   %b   %b    %b    %b",
-                 SW0, SW1, AND_OUT, OR_OUT, XOR_OUT, NAND_OUT, NOR_OUT);
+        $display(" %b   %b  |  %b   %b   %b    %b    %b    %b",
+                 SW0, SW1, AND_OUT, OR_OUT, XOR_OUT, NAND_OUT, NOR_OUT, XNOR_OUT);
 
         $stop;
 
